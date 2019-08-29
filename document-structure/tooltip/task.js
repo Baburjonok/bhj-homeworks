3b2a4f106aec085d2f71713dde369bc9f7ae8c16
Elena Bikh <baburjonok@mail.ru>
2019-08-29 16:06:59 +0500

    first commit tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,31 @@
+"use strict"
+
+let link = document.getElementsByClassName("has-tooltip");
+
+
+for (let i = 0; i < link.length; i++) {
+
+	let linkItem = link.item(i);
+
+    function callPrompt(eventClick) {
+	
+	    eventClick.preventDefault();	
+	
+	    let event = eventClick.target;
+		let element = document.createElement('div');
+		
+		let elementForDelete = document.getElementsByClassName("tooltip");
+		let elementForDeleteItem = elementForDelete.item(0);
+		
+		let parentOfElementForDelete = document.getElementsByTagName("body");
+		let parentOfElementForDeleteItem = parentOfElementForDelete.item(0);
+			
+		linkItem.insertAdjacentHTML("afterEnd", `<div class='tooltip tooltip_active'>${event.title}</div>`);
+		
+		if (elementForDeleteItem !== null ) {
+			parentOfElementForDeleteItem.removeChild(elementForDeleteItem);
+		}
+	}
+		
+	linkItem.addEventListener('click', callPrompt);
+}
