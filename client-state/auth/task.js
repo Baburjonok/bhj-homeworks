fab5a8c16176390dd1850a63498f15dd6bba712d
Elena Bikh <baburjonok@mail.ru>
2019-09-03 18:07:57 +0500

    first commit auth

---
+++ client-state/auth/task.js
@@ -0,0 +1,47 @@
+﻿"use strict"
+
+let form = document.getElementById("signin__form");
+
+let welcome = document.getElementById("welcome");
+
+let userId = document.getElementById("user_id");
+
+let signin = document.getElementById("signin");
+
+window.onload = function() {
+	
+	if (localStorage.user_id === undefined) {
+	    signin.className = "signin signin_active";
+	} else {
+	    welcome.className = "welcome welcome_active";
+    	userId.textContent = localStorage.user_id;
+	};
+};
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+    let formData = new FormData(form);
+	let request = new XMLHttpRequest();
+	
+    request.open("POST", "https://netology-slow-rest.herokuapp.com/auth.php");
+    request.send(formData);
+	
+	request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status == 200) {
+	        let answerJSON = request.responseText;
+		    let answerObject = JSON.parse(answerJSON);
+		
+		    if (answerObject.success === true) {
+			    welcome.className = "welcome welcome_active";
+			    localStorage.user_id = answerObject.user_id;
+			    userId.textContent = localStorage.user_id;
+			    signin.className = "signin";
+			} else 
+				
+			    if (answerObject.success === false) {
+			        alert("Неверные логин/пароль.");
+		        };
+	    };
+    };
+	
+});
