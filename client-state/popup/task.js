91efd3858faf855d7474736f6230552ce8e403e6
Elena Bikh <baburjonok@mail.ru>
2019-09-03 18:06:47 +0500

    first commit popup

---
+++ client-state/popup/task.js
@@ -0,0 +1,30 @@
+﻿"use strict"
+
+let close = document.getElementsByClassName("modal__close modal__close_times");
+
+let closeItem = close.item(0);
+
+let windowShow = document.getElementById("subscribe-modal");
+
+function closeWindow() {
+			
+	windowShow.className = "modal";
+	document.cookie = 'window=close';
+};
+
+window.onload = function() {
+	
+    let getCookie = (name) => {
+        let value = "; " + document.cookie;
+        let parts = value.split("; " + name + "=");
+        if (parts.length !== 2) {
+	    	windowShow.className = "modal modal_active";
+	    };
+    }; 
+
+getCookie("window");
+
+}
+
+
+closeItem.addEventListener('click', closeWindow);
